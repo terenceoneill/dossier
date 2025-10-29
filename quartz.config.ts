aa951e9642aaf84a090eacae433d6c8ed3aba622
Terence Oneill <oneillt@msu.edu>
2025-10-29 14:52:18 -0400

    Update baseUrl in config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dossier-2v4.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
