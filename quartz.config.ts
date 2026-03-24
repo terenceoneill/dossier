21ee162dd6d13d4013533f07511fff375a2ed658
terenceoneill <terenceoneill3@gmail.com>
2026-03-24 16:50:48 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Terence O'Neill - Dossier (Draft)",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
